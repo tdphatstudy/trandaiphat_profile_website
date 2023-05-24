799eb5420910a5e442913d070f6b5bae01e074cd
tdphatstudy <tdphat.study@gmail.com>
2023-05-24 23:25:12 +0700

    thêm Homepage

---
+++ pages/homepage.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PhatTran's Profile</title>
+        <link rel="stylesheet" href="../assets/css/global.css">
+        <link rel="stylesheet" href="../assets/css/header.css">
+        <link rel="stylesheet" href="../assets/css/footer.css">
+        <link rel="stylesheet" href="../assets/css/body.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="navbar-wrapper">
+                <div class="logo-website"> &lt; /&gt; PhatTran</div>
+                <div class="menu-wrapper">
+                    <div class="homepage-option action">Homepage</div>
+                    <div class="resume-option">Resume</div>
+                    <div class="project-option">Project</div>
+                    <div class="education-option">Education</div>
+                    <div class="notification-option">Notification</div>
+                    <div class="contact-option">Contact</div>
+
+                </div>
+            </div>
+            <div class="body-wrapper">
+                <div class="panel-info-wrapper">
+                    <div class="panel-info-content">
+                        <div class="name-info"><div class="name-text">> TRẦN ĐẠI PHÁT</div></div>
+                        <div class="job-info">Web Developer <span>&</span> Java Developer</div>
+                        <div class="description-info">Hello and welcome to my profile website! <br />
+                            I'm Tran Dai Phat, a final-year student at Saigon University, majoring in Information Technology. I have a strong passion for websites, databases, and operating systems. I always seek opportunities to challenge myself and grow in this field.
+                            During my university years, I have had the chance to work on various interesting and diverse projects. Through these experiences, I have developed problem-solving skills, creative thinking, and the ability to find effective solutions.
+                            I have identified some strengths in myself, such as being dedicated to my work, being eager to learn new knowledge, and being able to handle work pressures.
+                            I also have areas that need improvement, such as lack of confidence in a new environment. However, I have recognized this and I am making efforts to improve in this regard.</div>
+                            <div class="statistics-wrapper">
+                                <div class="project-statistics"><span>7+</span> Project Were Worked On.</div>
+                                <div class="programming-language-statistics"><span>4+</span> Programming Language Experienced.</div>
+                                <div class="hour-coding-statistics"><span>252+</span> Hours Spent On Programming.</div>
+                            </div>
+                    </div>
+                    <div class="panel-info-gadient"><div class="avatar-image"></div></div>
+                    
+                </div>
+            </div>
+            <div class="footer-wrapper"></div>
+        </div>
+    </body>
+</html>
